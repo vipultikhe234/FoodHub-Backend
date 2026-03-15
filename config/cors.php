94bba4510e6da5edd96557c62b2d93cccc4cdd15
vipultikhe234 <vipultikhe234@gmail.com>
2026-03-15 15:12:04 +0530

    added changes

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://food-hub-frontend-beryl.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
